f25f647da980251edef9135461b15e7d67ac681f
TheM0NKEY <jack.williams497@googlemail.com>
2019-11-18 23:04:54 +0000

    Start

---
+++ RHS-SlackBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RHS_SlackBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
